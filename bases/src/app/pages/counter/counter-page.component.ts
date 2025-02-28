ab0a1ceac724cced8c9baf26d98ec5f4925d12bb
Natalia Rosero <annroser@bancolombia.com.co>
2025-02-28 17:10:17 -0500

    Angular start

---
+++ bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+ @Component({
+    template: `
+    <h1> hola </h1>
+    <h2> CounterComponentPage </h2>
+    `
+ })
+export class CounterPageComponent{
+
+}
